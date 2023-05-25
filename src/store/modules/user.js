7acd04757dba065f02c7b4dc5e5a4bdf5ae19f88
NguyenTrungHieu <trunghieu220801@gmail.com>
2023-05-25 10:58:44 +0700

    final

---
+++ src/store/modules/user.js
@@ -0,0 +1,19 @@
+const state = {
+  token: "",
+  name: "",
+  email: "",
+  role: "",
+};
+const getters = {};
+const actions = {
+  login({ commit }, userInfo) {},
+};
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
